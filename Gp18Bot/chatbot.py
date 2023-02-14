7b101d4683e6bf2c6e082fd50ffd396f8d589d65
Huang Junwei <575734764wei@gmail.com>
2023-02-14 21:14:04 +0800

    homework update add /help command

--- Gp18Bot/chatbot.py
+++ Gp18Bot/chatbot.py
@@ -26,6 +26,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_command))
 
     # To start the bot:
     updater.start_polling()
@@ -43,6 +44,10 @@ def echo(update, context):
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Helping you helping you.')
+
+def hello_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Good day,' + str(context.args[0]))
 
 
 def add(update: Update, context: CallbackContext) -> None:
